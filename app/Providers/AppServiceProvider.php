4b87048ea309c03a1e9fc9495bc89e687583fda7
Budhi Octaviansyah <budioct@gmail.com>
2024-07-01 10:32:02 +0700

    add query log db

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        /**
+         * Debug Query
+         * ● Pada kasus tertentu, kadang kita ingin melakukan debugging SQL query yang dibuat oleh Laravel
+         * ● Kita bisa menggunakan DB::listen()
+         * ● DB::listen() akan dipanggil setiap kali ada operasi yang dilakukan oleh Laravel Database
+         * ● Kita bisa me-log query misalnya
+         * ● Kita bisa daftarkan DB::listen pada Service Provider
+         */
+
+        // DB::listen(callback) // kita akan dengar semua aktifitas DB Facade laravel
+        // QueryExecuted object yang menerima interaksi query di laravel
+        // Log::info() // semua aktifitas kita simpan dalam log.. nanti hasil log ada di directory ../storage/logs/laravel.log
+        DB::listen(function (QueryExecuted $query) {
+            Log::info($query->sql); // sql // properti yang mau kita simpan di log
+        });
     }
 }
